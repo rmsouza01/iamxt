edc25294db3e6bf091c294bcde90c289c846abdf
rmsouza01 <rmsouza@dca.fee.unicamp.br>
2014-10-21 13:14:27 -0200

    Including missing files of the toolbox

---
+++ iamxt/max_tree_c_01.h
@@ -0,0 +1,20 @@
+void counting_sort_c(int , unsigned char *, int **, int *);
+
+void canonicalize_c(int , unsigned char *, int , int *,
+                    int , int *);
+
+void union_find2d_c(int H,int W, int ,int , int *,int , int *,
+                    int , int *,int , int *, int ,
+                    unsigned char *);
+
+void computeNodeArray2d_c(int , int *,int , unsigned char *, int ,
+                          int * ,int , int , int *,
+                          int **, int *, int *);
+
+void union_find3d_c(int L,int M, int N, int ,int , int *,int ,
+                    int *, int , int *,int ,
+                    int *, int , unsigned char *);
+
+void computeNodeArray3d_c(int , int *,int , unsigned char *,
+                          int , int * ,int , int , int , int *,
+                          int **, int *, int *);
---
+++ iamxt/morph_tree_alpha_aux.h
@@ -0,0 +1,23 @@
+void get_descendants_aux_c(int node, int , int *, int , int *, int **, int *);
+
+void get_ancestors_aux_c(int node, int , int *,int **, int *);
+
+void get_children_aux_c(int ,  int *,int ,  int *, int **, int *);
+
+void get_sub_branches_aux_c(int , int *, int , int *,
+                            int , int *,int , int *,
+                            int **, int *);
+
+void contract_dr_2d_aux_c(int , int *, int , int *,
+int , int *, int , int *,int , int *,
+int , int *,int , int *, int , int *,
+int , int *, int , int , int *);
+
+void contract_dr_3d_aux_c(int , int *, int , int *, int ,
+int *, int , int *,int , int *,int ,
+int *,int , int *, int , int *,int ,
+int *, int , int *, int , int *, int ,
+int , int , int *);
+
+void prune_aux_c(int , int *, int , int *, int ,
+                 int *, int , int *);
