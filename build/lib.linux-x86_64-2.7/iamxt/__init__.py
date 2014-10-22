bf0202f245d2e2ad55e4c2f07bd0f96490338989
rmsouza01 <rmsouza@dca.fee.unicamp.br>
2014-10-22 15:15:57 -0200

    Removed garbage files

--- build/lib.linux-x86_64-2.7/iamxt/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-# Module MainPage
-
-from aux import create1DImage, extrema2attribute, generateGraph,  se2off, SSIMIndex
-from max_tree_alpha import MaxTreeAlpha
-from morph_tree_alpha import MorphTreeAlpha
-
-
